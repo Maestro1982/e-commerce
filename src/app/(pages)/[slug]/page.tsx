035a1b1d5707d8b7457325b91db64493d454375e
Maestro1982 <kris.tote@telenet.be>
2023-12-12 11:09:45 +0100

    modify .eslintrc.js and page.tsx in src app (pages) [slug] folder

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -1,23 +1,16 @@
+import React from 'react'
 import { Metadata } from 'next'
 import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
 
 import { Category, Page } from '../../../payload/payload-types'
 import { staticHome } from '../../../payload/seed/home-static'
-
 import { fetchDoc } from '../../_api/fetchDoc'
 import { fetchDocs } from '../../_api/fetchDocs'
-
 import { Blocks } from '../../_components/Blocks'
-import { Hero } from '../../_components/Hero'
 import { Gutter } from '../../_components/Gutter'
-
-import Categories from '../../_components/Categories'
-import Promotion from '../../_components/Promotion'
-
+import { Hero } from '../../_components/Hero'
 import { generateMeta } from '../../_utilities/generateMeta'
-
-import classes from './index.module.scss'
 
 // Payload Cloud caches all files through Cloudflare, so we don't need Next.js to cache them as well
 // This means that we can turn off Next.js data caching and instead rely solely on the Cloudflare CDN
@@ -26,6 +19,11 @@ import classes from './index.module.scss'
 // See https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
+
+import Categories from '../../_components/Categories'
+import Promotion from '../../_components/Promotion'
+
+import classes from './index.module.scss'
 
 export default async function Page({ params: { slug = 'home' } }) {
   const { isEnabled: isDraftMode } = draftMode()
@@ -62,12 +60,10 @@ export default async function Page({ params: { slug = 'home' } }) {
   const { hero, layout } = page
 
   return (
-    <>
+    <React.Fragment>
       {slug === 'home' ? (
         <section>
-          <Gutter className={classes.home}>
-            <Hero {...hero} />
-          </Gutter>
+          <Hero {...hero} />
 
           <Gutter className={classes.home}>
             <Categories categories={categories} />
@@ -83,7 +79,7 @@ export default async function Page({ params: { slug = 'home' } }) {
           />
         </>
       )}
-    </>
+    </React.Fragment>
   )
 }
 
